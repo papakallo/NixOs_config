f2d5157d0f95e4980e9ebd293782b9d94fd0ad2e
Sergiusz Pyskowacki <paviveerar@gmail.com>
2025-09-02 17:59:18 +0200

    add steam

--- configuration.nix
+++ configuration.nix
@@ -146,6 +146,13 @@
     hunspellDicts.pl_PL 
  ];
 
+ programs.steam = {
+  enable = true;
+  remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+ };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
