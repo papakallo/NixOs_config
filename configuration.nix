e841ecf97411340b33c46bb68a912a87f8ccfd97
Sergiusz Pyskowacki <paviveerar@gmail.com>
2025-11-09 18:07:01 +0100

    nix-ld

--- configuration.nix
+++ configuration.nix
@@ -158,7 +158,8 @@
  #docker
  virtualisation.docker.enable = true;
  virtualisation.docker.storageDriver = "btrfs"; 
-  
+
+ programs.nix-ld.enable = true;  
  
 # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
