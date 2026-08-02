d731d8c132089a5e03e15f9af03b9346dca6bc25
papakallo <paviveerar@gmail.com>
2026-08-02 18:43:42 +0200

    turn off firewall

--- configuration.nix
+++ configuration.nix
@@ -162,10 +162,10 @@
   services.openssh.enable = true;
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 8384 22000 ];
-  networking.firewall.allowedUDPPorts = [ 22000 21027 ];
+  # networking.firewall.allowedTCPPorts = [ 8384 22000 ];
+  # networking.firewall.allowedUDPPorts = [ 22000 21027 ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
