8bac05f29202df054953991149a5b1a83e4d1593
Sergiusz Pyskowacki <125501409+papakallo@users.noreply.github.com>
2026-09-13 14:59:17 +0200

    Dendritic (#3)
    * feat(nix): start using dendritic pattern
    
    * remove unused module
    
    * feat(sway): move sway configuration as a separate module
    
    * update: clean comments and garbage

---
+++ modules/hosts/light/home-manager.nix
@@ -0,0 +1,110 @@
+{ self, inputs, ... }: {
+    flake.nixosModules.lightHome = { pkgs, ... }:
+{
+  imports = [
+    inputs.home-manager.nixosModules.home-manager
+  ];
+
+  home-manager.useGlobalPkgs = true; # forces Home Manager to use the system's pkgs (which knows about Flake inputs) instead of trying to evaluate import <nixpkgs>
+  home-manager.useUserPackages = true; # It installs user packages into /etc/profiles instead of ~/.nix-profile. This keeps your environment cleaner and ties user packages directly to the system generations.
+
+
+  home-manager.sharedModules = [
+        self.homeModules.sway
+  ];
+
+
+  home-manager.users.papakallo = { config, lib, ... }: {
+    /* The home.stateVersion option does not have a default and must be set */
+    home.stateVersion = "26.05";
+
+    home.packages = with pkgs; [
+      unzip
+      vlc
+      obs-studio
+      gimp3-with-plugins
+      wget
+      vscode
+      libreoffice-qt
+      hunspell
+      hunspellDicts.pl_PL 
+      spotify
+      dosbox-staging
+      openmw
+      kdePackages.kate
+      telegram-desktop
+      discord-ptb
+      distrobox
+      atlauncher
+      feh
+      kicad
+      # temporarily removed, because the branch is very unstable
+      # freecad
+      nomachine-client
+      signal-desktop
+      element-desktop
+      kitty
+      ddcutil
+      findutils
+      gawk
+      grim
+      slurp
+      sway-contrib.grimshot
+    ];
+
+    programs.zsh = {
+        enable = true;
+    };
+
+    programs.git = {
+      enable = true;
+      settings.user.name = "papakallo";
+      settings.user.email = "paviveerar@gmail.com";
+    };
+
+    programs.vscode = {
+      enable = true;
+      profiles.default.extensions = with pkgs.vscode-extensions; [
+        dracula-theme.theme-dracula
+        vscodevim.vim
+        yzhang.markdown-all-in-one
+	zainchen.json
+        ms-python.python
+        ms-python.debugpy
+	ms-vscode.cpptools
+	ms-vscode.cmake-tools
+#        ms-vscode.cpptools-themes
+	ms-vscode-remote.remote-ssh
+	
+      ];
+    };
+
+    programs.neovim = {
+      enable = true;
+   };
+
+    xdg.configFile."nvim".source = "${inputs.nvim-config}";
+
+    # ricing
+    programs.waybar.enable = true;
+
+    services.swayidle.enable = true;
+
+    programs.swaylock = {
+      enable = true;
+      settings = {
+        color = "808080";
+        font-size = 24;
+        indicator-idle-visible = false;
+        indicator-radius = 100;
+        line-color = "ffffff";
+        show-failed-attempts = true;
+      };
+    };
+
+
+    };
+
+};
+
+}
