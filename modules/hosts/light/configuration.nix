84370405c56c842894c3aa063fe8dc620b0b4632
papakallo <paviveerar@gmail.com>
2026-09-23 18:17:39 +0200

    remove default commented features

--- modules/hosts/light/configuration.nix
+++ modules/hosts/light/configuration.nix
@@ -59,11 +59,6 @@ in
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   networking.hostName = "light";
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   networking.networkmanager.enable = true;
   hardware.bluetooth.enable = true;
@@ -159,19 +154,7 @@ in
 
 programs.zsh.enable = true;
 
-# Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
 
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ 8384 22000 ];
-  # networking.firewall.allowedUDPPorts = [ 22000 21027 ];
-  # Or disable the firewall altogether.
   networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
